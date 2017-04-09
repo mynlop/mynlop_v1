1544690004dc537600c34261ae62fa80f34822c2
Mynor Lopez <mynlop15@gmail.com>
2017-04-09 13:39:22 -0600

    se agrego animacion de barra lateral, se agrego informaion de proyecto, y hover, incluyendo el responsive

--- js/script.js
+++ js/script.js
@@ -8,4 +8,31 @@ $(document).ready(function(){
              $(".areaInfo").css("left", "-100%");
         }  
     });
+    $(".carta").click(function(){
+    	$(".personal").css("justify-content","space-around");
+    	$(".foto").css("width","25%");
+    	$(".info").css("display","none");
+    	$(".infoProyecto").css("display","block");
+    });
+    $(".cerrarInfo").click(function(){
+    	$(".foto").css("width","70%");
+    	$(".info").css("display","block");
+    	$(".infoProyecto").css("display","none");
+    });
+    var mediaQuery = window.matchMedia("(max-width: 500px)");
+    if(mediaQuery.matches){
+	    $(".carta").click(function(){
+    		$(".personal").css("justify-content","space-around");
+    		$(".foto").css("width","25%");
+    		$(".info").css("display","none");
+    		$(".infoProyecto").css("display","block");
+    		 $(".areaInfo").css("left", "0%")
+    	});
+	    $(".cerrarInfo").click(function(){
+	    	$(".foto").css("width","70%");
+	    	$(".info").css("display","block");
+	    	$(".infoProyecto").css("display","none");
+	    	$(".areaInfo").css("left", "-100%");
+	    });
+    }
 });
